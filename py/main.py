5bb73e02ba77af115a7a4b2ad7e99ec58e57d13a
TwinkletoesZen <contact@twinkletoeszen.com>
2021-06-04 22:06:48 -0700

    Catch Up_

--- py/main.py
+++ py/main.py
@@ -1,40 +1,66 @@
 import json
-
+import os.path
 
 print("Welcome to TwinkleMoneyTree")
 
-Currnet_Saving_Money_Amount = 0
-Currnet_Queqing_Money_Amount = 0
-
-print(Currnet_Queqing_Money_Amount and Currnet_Saving_Money_Amount)
-
-if (Currnet_Saving_Money_Amount + Currnet_Queqing_Money_Amount == 0):
-  print("Please Enter your savings account amount: ")
-  Currnet_Saving_Money_Amount = int(input(": "))
-
-  print("Please Enter your queqing account amount: ")
-  Currnet_Queqing_Money_Amount = int(input(": ")) 
-
-Currnet_Saving_Money_Amount_str = {"Saving": {"Amount":"$" + str(Currnet_Saving_Money_Amount)}}
-with open("AmountofMoney.json", "a") as write:
-  json.dump(Currnet_Saving_Money_Amount_str, write, indent=4)
+class Tracker():
 
 
-Currnet_Queqing_Money_Amount_str = {"Queqing": {"Amount":"$" + str(Currnet_Queqing_Money_Amount)}}
-with open("AmountofMoney.json", "a") as write:
-  json.dump(Currnet_Queqing_Money_Amount_str, write, indent=4)
+  def Inital_Setup():
+
+    global Currnet_Saving_Money_Amount
+    global UserName
+    global Currnet_Queqing_Money_Amount
+    global AccountStatus
+
+
+    print("Please Enter your savings account amount: ")
+    Currnet_Saving_Money_Amount = int(input(": "))
+
+    print("Please Enter your queqing account amount: ")
+    Currnet_Queqing_Money_Amount = int(input(": ")) 
+
+    print("Please Enter who holds these accounts: ")
+    UserName = input(": ")
+
+    print("Please Enter the Accounts Status: ")
+    AccountStatus = input(": ")
+
+
+  def Writing_Data():
+    Currnet_Saving_Money_Amount_str = {'Saving': {'Amount': Currnet_Saving_Money_Amount, 'Owner': UserName, 'Status': AccountStatus}}
+    with open("log/Saving.json", "w") as write_saving:
+      json.dump(Currnet_Saving_Money_Amount_str, write_saving, indent=4)
+
+    Currnet_Queqing_Money_Amount_str = {'Queqing': {'Amount': Currnet_Queqing_Money_Amount, 'Owner': UserName, 'Status': AccountStatus}}
+    with open("log/Queqing.json", "w") as write_queqing:
+      json.dump(Currnet_Queqing_Money_Amount_str, write_queqing, indent=4)
 
 
 
 
+Check_For_Saving = (os.path.exists("log/Saving.json"))
+Check_For_Queqing = (os.path.exists("log/Queqing.json"))
 
-# data = {"president": {"name": "Zaphod Beeblebrox","species": "Betelgeusian"}}
+if Check_For_Queqing & Check_For_Saving == True:
+  pass
+else:
+  Tracker.Inital_Setup()
 
-# with open("data_file.json", "w") as write_file:
-#     json.dump(data, write_file, indent=4 )
+
+Command = input("How can I help you? \n : ").upper()
+
+while True:
+  if Command == "WRITEDATA":
+    Tracker.Writing_Data()
+
+  elif Command == "HELP" or "MAN":
+    with open("help/help.txt", "rb") as Read_Help:
+      print(Read_Help)
 
 
-# class Finance_Recorder:
-
-#   def Input_Expense():
-    
+  elif Command == "BREAK" or "QUIT" or "STOP" or "EXIT":
+    break
+    quit()
+  else:
+    print("Wrong Command")
